341c253f75ec7e42bb2fb11d662cefddb3a1dc54
ahmetaric <ahmet.aric1965@gmail.com>
2022-10-16 12:31:04 +0300

    v-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,15 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
-  const navigate = useNavigate()
-  const user = true
-  const handleLogout=()=>{
-     //! clear user data
-     navigate("login")
-  }
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
+  const handleLogout = () => {
+    //! clear user data
+    navigate("login");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
